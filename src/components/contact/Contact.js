de9ace0bc4ee778158eb789ff03b55e74e05494a
Kidti <you@example.com>
2022-04-11 20:48:28 +0200

    contactPage

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -7,6 +7,12 @@ const Contact = ({scrollToTop, showButton}) => {
       <div className="contact-box">
         <h2 id="contact">Contact</h2>
         {showButton && <img onClick={scrollToTop} src={arrow} alt="up-arrow" />}
+        <div className="social-icons">
+          <p>LinkedIn</p>
+          <p>Github</p>
+          <p></p>
+        </div>
+        <p>Szasz-Debreczeni Kitti <span style={{color:"var(--operamauve)"}}>@2022</span></p>
       </div>
     </div>
   );
