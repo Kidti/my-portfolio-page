a2e5b2aef1ddd4aa8e4bb597332d9168485b5cb7
Kidti <you@example.com>
2022-04-11 17:45:31 +0200

    scrollToTopBtn

--- src/App.js
+++ src/App.js
@@ -8,21 +8,41 @@ import Modal from "./components/modal/Modal";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
+  const [showButton, setShowButton] = useState(false);
 
+  // for preventing scrolling when the modal is open
   useEffect(() => {
     if (showModal) {
-      document.body.style.overflow = 'hidden'
-    }else{
-      document.body.style.overflow = ''
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
     }
-  }, [showModal])
+  }, [showModal]);
+
+  // This function will scroll the window to the top
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <div className="App">
       <Navbar setShowModal={setShowModal} showModal={showModal} Modal={Modal} />
       <LandingPage />
       <About />
-      <Contact />
+      <Contact scrollToTop={scrollToTop} />
     </div>
   );
 }
