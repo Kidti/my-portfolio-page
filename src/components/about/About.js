721257a5e220f9d188be89a2c1f4908f663c1c20
Kidti <you@example.com>
2022-04-21 19:25:49 +0200

    .

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,23 +1,39 @@
 import "./About.css";
-import icon from "./icon.png";
-import mypic from "./mypic.png"
-import skill from "./skills.png"
+import lightbulb from "./lightbulb-icon.png";
+import scale from "./honesty-scale.png";
+import responsive from "./responsive.png";
+import learn from "./learn.png";
+import mypic from "./mypic.png";
+import skill from "./skills.png";
 
 const About = () => {
   return (
     <div className="about-page" id="about-me">
-        <h1>About Me</h1>
-      
+      <h1>About Me</h1>
       <div className="about-box">
-
-          <div className="icon-box icon-1"><img src={icon} alt="icon" /><p>Transparent</p></div>
-          <div className="icon-box icon-2"><img src={icon} alt="icon" /><p>Honest</p></div>
-          <div className="icon-box icon-3"><img src={icon} alt="icon" /><p>Responsive</p></div>
-          <div className="icon-box icon-3"><img src={icon} alt="icon" /><p>Intuitive</p></div>
+        <div className="icon-box icon-1">
+          <img src={learn} alt="icon" />
+          <h3>Egaer to learn</h3>
+          <p>Constant learning and developing is really motivating for me.</p>
+        </div>
+        <div className="icon-box icon-2">
+          <img src={scale} alt="icon" />
+          <h3>Transparent</h3>
+          <p>Transparency and trust make it easier to work with people.</p>
+        </div>
+        <div className="icon-box icon-3">
+          <img src={responsive} alt="icon" />
+          <h3>Responsive</h3>
+          <p>It's important that layouts work on any device, big or small.</p>
+        </div>
+        <div className="icon-box icon-3">
+          <img src={lightbulb} alt="icon" />
+          <h3>Intuitive</h3>
+          <p>Strong desire for easy to use, intuitive UX/UI.</p>
+        </div>
 
         <div className="textbox">
-
-          <img src={mypic} alt="this-is-me"/>
+          <img src={mypic} alt="this-is-me" />
           <p>
             Something interesting about me, some text, but I don't know yet what
             should I write about myself, I am not a very interesting person,
@@ -25,13 +41,11 @@ const About = () => {
             agree honey, cuz this year will be our year. Maybe we are not the
             sharpest knife in the drawer but we sure are the funniest.
           </p>
-
         </div>
 
         <div className="skills">
-          <img src={skill} alt="skills-info"/>
+          <img src={skill} alt="skills-info" />
         </div>
-
       </div>
     </div>
   );
