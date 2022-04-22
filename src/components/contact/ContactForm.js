47c299fc0335fd845ce1dc6b69300200385e5788
Kidti <you@example.com>
2022-04-22 15:35:59 +0200

    textarea

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -3,13 +3,13 @@ import "./ContactForm.css";
 const ContactForm = () => {
   return (
     <div className="contact-form">
-        <h3 style={{color:"var(--lightseagreen)"}}>In case you want to contact me:</h3>
+        <h3 style={{color:"var(--lightseagreen)"}}>Feel free to contact me &#128522;</h3><br/>
       <form action="https://formsubmit.co/kitti.deb@gmail.com" method="POST">
         <input type="hidden" name="_subject" value="New e-mail from website!" />
         <input type="text" name="name" placeholder="Name" required />
         <input type="hidden" name="_next" value="http://localhost:3000/" />
         <input type="email" name="email" placeholder="Enter e-mail" required />
-        <input type="text" name="message" placeholder="Your message" required />
+        <textarea type="text" name="message" placeholder="Your message" maxLength="300" required />
         <button type="submit">Send</button>
       </form>
     </div>
